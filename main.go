591d23bfc7dc160e30b07a62719b23efc2485025
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-08-07 16:02:13 -0300

    implements /hello handler

--- main.go
+++ main.go
@@ -6,9 +6,21 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method is not supported", http.StatusNotFound)
+	}
+	fmt.Fprintf(w, "Hello World!")
+}
+
 func main() {
 	var fileServer http.Handler = http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	err := http.ListenAndServe(":8080", nil)
