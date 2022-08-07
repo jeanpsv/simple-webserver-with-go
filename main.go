8f68d2850157e72f55748b89c168a5960fe7ac22
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2022-08-07 16:06:28 -0300

    implements form handler

--- main.go
+++ main.go
@@ -17,10 +17,24 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "ParserForm() err: %v", err)
+		return
+	}
+	fmt.Fprint(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func main() {
 	var fileServer http.Handler = http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	err := http.ListenAndServe(":8080", nil)
